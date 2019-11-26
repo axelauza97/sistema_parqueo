787066a0930d87139afe48bf2ecc8fa3f92f4c3a
Neythan Levanest <neythan.astro@gmail.com>
2019-11-25 23:29:15 -0500

    actualización de funciones ingresar y remover vehículos contemplando el nuevo formato en el cual se asigna en el while del main las variables de argumentos a0-a3

--- version_ensamblador.asm
+++ version_ensamblador.asm
@@ -1,62 +1,66 @@
+.data  
+	park: .word 0:20 # Arreglo de autos
+	park_size: .word 20 
+    placa_base: .word 000000 
+	prompt: .asciiz "Ingrese una opcion\n"
+	prompt2: .asciiz "1.- Observar parqueos ocupados\n"
+	prompt3: .asciiz "2.- Ingresar auto a parqueo\n"
+	prompt4: .asciiz "3.- Retirar auto de parqueo\n"
+  	prompt5: .asciiz "4.- Salir"
+    prompt6: .asciiz "5.- Ingresando auto..."
+.text 
+	main:
+        la $s0, park # Cargo la dir del parqueo
+        la $s1, park_size # Cargo la dir del size parqueo
+        lw $s1, 0($s1) # Cargo size
+        lw $s2, placa_base 
+        addi $t0, $zero,$zero
+        syscall
+        
+        # considerando que
+                    # addi $t3, $v1, 0			# guardar input
+					# addi $a0, $t3, 0			# guardar valores para funcion	los argumentos corresponden a los arreglos y a la entrada de la placa							
+					# addi $a1, $s0, 0          
+					# addi $a2, $s1, 0
+                    # addi $a3, $s2, 0
 
-#for (i=0; i<a:i++)
-#	for (j=0; j<b: j++)
-		D[4*j]= i+j
+    # Funcion ingresar autos
+    ingresar_auto:
+            addi $t0, $zero, $zero       # inicializar el bucle for en 0
+            slt $t1, $t0, $a2            
+         
+            beq $t1, $zero, EXIT         # Verificar si hay un espacio disponible en cada registro del parking
+            mul $t2, $t0, $a1
+            beq $t2, $a3, asignar_auto   # Aumentar el contador en 1 si no hay disponibilidad
+               addi $t0, $t0, 1
+               addi $v0, $zero, 1
+        asignar_auto:                    # Asingar auto en un espacio vacío 
+            add $t2, $t2, $a0
+            add $a1, $a1, $t2
+            addi $v0, $zero, 0
+            # llamar a funciones para imprimir a partir de aquí
+            j ExitIA
+        ExitIA:
+            jr $ra
+            
+  
+	        	
 
-#a -> $s0
-#b -> $s1
-#i -> $t0
-#j -> $t1
-#D -> $s2
-
-#placa -> $t0
-#max_auto -> $t1
-#0 -> $t2
-
-
-max_auto: .word 10
-placa_base: .word 000000
-
-
-main:
-	#Funcion ingresar autos
-	#inicializacion de parametros
-ingresar_auto:
-	addi $sp, $sp,-8  
-	sw $t1, 4($sp)
-	sw $t0, 0($sp)
-	addi $t2, $zero, $zero
-	slti $t3, $t2, max_auto
-Loop1: 
-	beq $t3, $zero, EXIT
-	mul $t4, $t1, $t2
-	bne $t4, placa_base, L1
-	add $t5, $t5, $t0
-	add $t4, $t4, $t5
-L1:
-	addi $t2, $t2, 1
-	lw $t1,0($sp)
-	lw $t1,4($sp)
-	addi $sp, $sp, 8
-	
-#Funcion remover autos
-
-remover_auto:
-	addi $sp, $sp,-8  
-	sw $t1, 4($sp)
-	sw $t0, 0($sp)
-	addi $t2, $zero, $zero
-	slti $t3, $t2, max_auto
-Loop2: 
-	beq $t3, $zero, EXIT
-	mul $t4, $t1, $t2
-	bne $t4, $t0, L1
-	add $t5, $t5, placa_base
-	add $t4, $t4, $t5
-L2:
-	addi $t2, $t2, 1
-	lw $t1,0($sp)
-	lw $t1,4($sp)
-	addi $sp, $sp, 8
-exit:
+    remover_auto:
+            addi $t0, $zero, $zero       # inicializar el bucle for en 0
+            slt $t1, $t0, $a2            
+         
+            beq $t1, $zero, EXIT         # Verificar si hay un espacio disponible en cada registro del parking
+            mul $t2, $t0, $a1
+            beq $t2, $a0, remover_auto   # Comparar el valor de la placa tomado en la entrada y almacenado en a0 para verificar si está en el registro
+               addi $t0, $t0, 1
+               addi $v0, $zero, 1
+        remover_auto:                    # Liberar espacio 
+            addi $t2, $t2, 0
+            add $a1, $a1, $t2
+            addi $v0, $zero, 0
+            # llamar a funciones para imprimir a partir de aquí
+            j ExitIA
+        ExitIA:
+            jr $ra
 
